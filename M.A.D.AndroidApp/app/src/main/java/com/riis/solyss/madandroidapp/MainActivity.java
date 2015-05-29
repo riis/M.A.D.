27c4bc5cfd4b2552327d12d46c63cceaa2f580e7
Scub3d <scubedtechnologiesllc@gmail.com>
2015-05-29 14:59:39 -0400

    Final commit
    Sad face

--- M.A.D.AndroidApp/app/src/main/java/com/riis/solyss/madandroidapp/MainActivity.java
+++ M.A.D.AndroidApp/app/src/main/java/com/riis/solyss/madandroidapp/MainActivity.java
@@ -1,18 +1,49 @@
 package com.riis.solyss.madandroidapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 //hello there fools
 public class MainActivity extends ActionBarActivity {
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private  Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameField = (EditText)findViewById(R.id.emailLoginField);
+        passwordField = (EditText)findViewById(R.id.passwordLoginField);
+        loginButton = (Button)findViewById(R.id.loginButton);
+
     }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.loginButton) {
+            if(passwordField.length() > 7 && isValidEmailAddress) {
+                // Send a request to our server to see if such a user exists
+
+            }
+
+
+        }
+    }
+
+
+
+
+
+
 
 
     @Override
@@ -36,4 +67,16 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
+    }
+
 }
